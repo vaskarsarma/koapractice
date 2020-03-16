be4583c3e2ac499b10e718f663683f5aa679424b
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-03-16 11:12:29 +0400

    Added more test case and routes

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ const Router = require('koa-router');
 
 const router = new Router();
 
+require('./routes/basic')({ router })
+
+const dogRouter= new Router({
+    prefix: '/dogs'
+});
+
+require('./routes/dogs')({
+    dogRouter
+});
+
+
 const logger = require('koa-logger');
 
+/*
 router.get('/', (ctx, next) => {
-    ctx.body = "Hello Vaskar Sarma!";
+    console.log(ctx);
+    ctx.body=ctx;
 });
+*/
 
 app.use(async (ctx, next) => {
     try {
@@ -30,6 +44,9 @@ app.use(async (ctx, next) => {
 app.use(logger());
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.use(dogRouter.routes());
+app.use(dogRouter.allowedMethods());
 
 const server = app.listen(3000, () => {
     console.log(`Server started on port 3000`);
